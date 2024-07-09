6ae53746971045a086fa3988e4c635100a8c7d75
joe <joenasr@email.com>
2024-07-09 19:01:32 +0300

    second commit

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -7,9 +7,9 @@ const ProductCard = ({ product }: {product: Product}) => {
     <div className="lg:w-1/4 md:w-1/2 p-4">
       <div className="border rounded-lg overflow-hidden">
         {product.imageURL ? (
-          <img className="w-full" src={product.imageURL} alt={product.name} />
+          <img className="w-full object-cover" src={product.imageURL} alt={product.name} />
         ) : (
-          <img className="w-full" src={process.env.PUBLIC_URL + '/images/placeholder.png'} alt={product.name} />
+          <img className="w-full object-cover" src={process.env.PUBLIC_URL + '/images/placeholder.png'} alt={product.name} />
         )}
         <div className="p-4">
           <h6 className="font-semibold">{product.name}</h6>
